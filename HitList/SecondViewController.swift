f9d07db132eac8b9862d4e9961b72a30e2db9c04
Kpatena <kamilpatena1221@gmail.com>
2016-03-31 19:29:02 -0700

    Sample app

---
+++ HitList/SecondViewController.swift
@@ -0,0 +1,74 @@
+import UIKit
+import CoreData
+
+class SecondViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //Change “names” to “people” and [String] to [NSManagedObject]
+    var names = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "\"The Non-Core Data List\""
+        tableView.registerClass(UITableViewCell.self,
+            forCellReuseIdentifier: "Cell")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addName(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "New Name",
+            message: "Add a new name",
+            preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+            style: .Default,
+            handler: { (action:UIAlertAction) -> Void in
+                
+                let textField = alert.textFields!.first
+                self.names.append(textField!.text!)
+                self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+            animated: true,
+            completion: nil)
+        
+    }
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return names.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell =
+            tableView.dequeueReusableCellWithIdentifier("Cell")
+            
+            cell!.textLabel!.text = names[indexPath.row]
+            
+            return cell!
+    }
+
+    
+}
